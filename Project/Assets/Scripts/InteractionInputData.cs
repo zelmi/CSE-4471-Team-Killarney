81c76f28c3aef86245e5e80edb8d43808938aaca
Speno <gorgonfire@gmail.com>
2021-03-25 17:19:08 -0400

    Interaction System Implemented (needs to interface with player controller still)

---
+++ Project/Assets/Scripts/InteractionInputData.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "InteractionInputData", menuName = "Interactions/InputData")]
+public class InteractionInputData : ScriptableObject
+{
+    private bool p_interactPress;
+    //private bool p_interactRelease;
+
+    public bool InteractPress {
+        get => p_interactPress;
+        set => p_interactPress = value;
+    }
+
+    /*public bool InteractRelease {
+        get => p_interactPress;
+        set => p_interactRelease = value;
+    }*/
+
+    public void Reset(){
+        p_interactPress = false;
+        //p_interactRelease = false;
+    }
+}
