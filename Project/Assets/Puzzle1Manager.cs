821bf0227eb79b990455b5fbb3d404affb1b2935
Veronica Thai <thai.53@buckeyemail.osu.edu>
2021-03-30 14:56:58 -0400

    Puzzle 1

---
+++ Project/Assets/Puzzle1Manager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using static UnityEngine.InputSystem.InputAction;
+
+public class Puzzle1Manager : MonoBehaviour
+{
+    public TMP_InputField email;
+    public TMP_InputField password;
+
+    //Probably should be private readonly, but we still need to figure out the final password, so leave public to change and test in Unity for now
+    public string expectedEmail;
+    public string expectedPassword;
+
+    public void OnClick()
+    {
+        if (email.text == expectedEmail && password.text == expectedPassword)
+        {
+            //Disable interaction
+            password.interactable = false;
+
+            //Debug print for now, tie to puzzle 6 later
+            Debug.Log("Correct password");
+        }
+    }
+}
