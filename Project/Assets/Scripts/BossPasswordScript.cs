c1988f07272f120b0adbb14777b0cb604c9c02e4
ryan scott <scott.2081@osu.edu>
2021-04-01 14:56:06 -0400

    Boss password puzzle

---
+++ Project/Assets/Scripts/BossPasswordScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using static UnityEngine.InputSystem.InputAction;
+
+public class BossPasswordScript : MonoBehaviour
+{
+    public TMP_InputField password;
+    
+    //Probably should be private readonly, but we still need to figure out the final password, so leave public to change and test in Unity for now
+    public string expectedPassword;
+
+
+    void Update() {
+        // if (Input.GetKeyDown(KeyCode.Return)) {
+        //     OnClick();
+        // }
+    }
+
+    public void OnClick()
+    {
+        if (password.text == expectedPassword)
+        {
+            //Disable interaction
+            password.interactable = false;
+
+            //Go to email puzzle
+            PuzzleSceneManager.SceneSwitch("GameScene");
+        }
+        password.text = "";
+    }
+}
