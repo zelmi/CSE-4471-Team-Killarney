4460aeb4e7b3476432f6a25633d40a71acb65e66
Speno <gorgonfire@gmail.com>
2021-04-02 17:54:08 -0400

    started interactions and continued populating main scene

---
+++ Project/Assets/Scripts/Interactables/System/HoverUIController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class HoverUIController : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI hoverText;
+
+    public void SetHoverMessage(string message) {
+        hoverText.SetText(message);
+    }
+
+    public void ResetUI(){
+        hoverText.SetText("");
+    }
+}
---
+++ Project/Assets/Scripts/Puzzle5PictureManager.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Puzzle5PictureManager : MonoBehaviour
+{
+    public void LeavePicture(){
+        PuzzleSceneManager.ExitPuzzle();
+    }
+}
