eb90a5ce4302db2839e7eca3b0a58ee35a687485
Veronica Thai <thai.53@buckeyemail.osu.edu>
2021-04-02 00:39:31 -0400

    Puzzle 6 email window

--- Project/Assets/Scripts/Puzzle6Manager.cs
+++ Project/Assets/Scripts/Puzzle6Manager.cs
@@ -4,9 +4,30 @@ using UnityEngine;
 using TMPro;
 using System;
 using UnityEngine.UI;
+using System.Xml;
 
 public class Puzzle6Manager : MonoBehaviour
 {
+    public class Message {
+        public string senderOrRecipient;
+        public string subject;
+        public string time;
+        public string text;
+
+        public Message(string senderOrRecipient, string subject, string time, string text)
+        {
+            this.senderOrRecipient = senderOrRecipient;
+            this.subject = subject;
+            this.time = time;
+            this.text = text;
+        }
+    }
+    //Message data
+    private List<Message> inboxData;
+    private List<Message> sentData;
+    private List<Message> spamData;
+    private List<Message> trashData;
+
     //Send button
     public Button sendButton;
 
@@ -24,7 +45,149 @@ public class Puzzle6Manager : MonoBehaviour
     public int subjectMatchCountTarget;
     public int messageMatchCountTarget;
 
-    public void OnClick()
+    //Email GameObject references
+    public GameObject emailComposeWindow;
+    public GameObject emailMessageList;
+    public GameObject emailMessageText;
+
+    //Message item prefab for instantiating list
+    public GameObject message;
+
+    void Start()
+    {
+        InitializeMessages();
+    }
+
+    public void OnClickEmailClose()
+    {
+        emailMessageText.SetActive(false);
+    }
+    public void OnClickComposeClose()
+    {
+        emailComposeWindow.SetActive(false);
+    }
+
+    public void OnClickEmailCategory(string type)
+    {
+        List<Message> currentData = inboxData;
+
+        switch (type)
+        {
+            case "Sent":
+                currentData = sentData;
+                break;
+            case "Spam":
+                currentData = spamData;
+                break;
+            case "Trash":
+                currentData = trashData;
+                break;
+        }
+
+        InstantiateMessages(currentData);
+    }
+
+    public void EmailItemEvent(List<Message> sourceList, int index)
+    {
+        emailMessageText.SetActive(true);
+
+        //Special case email was from sent
+        if (sourceList == sentData)
+        {
+            emailMessageText.transform.GetChild(0).GetChild(0).GetComponent<TMP_Text>().text = "hr.alice@bmail.com";
+            emailMessageText.transform.GetChild(1).GetChild(0).GetComponent<TMP_Text>().text = sourceList[index].senderOrRecipient;
+
+        } else
+        {
+            emailMessageText.transform.GetChild(0).GetChild(0).GetComponent<TMP_Text>().text = sourceList[index].senderOrRecipient;
+            emailMessageText.transform.GetChild(1).GetChild(0).GetComponent<TMP_Text>().text = "hr.alice@bmail.com";
+        }
+
+        emailMessageText.transform.GetChild(2).GetChild(0).GetComponent<TMP_Text>().text = sourceList[index].subject;
+        emailMessageText.transform.GetChild(3).GetChild(0).GetComponent<TMP_Text>().text = sourceList[index].text.Replace("~", Environment.NewLine);
+    }
+
+    void InstantiateMessages(List<Message> messageList)
+    {
+        //Clear list
+        for(int i = 0; i < emailMessageList.transform.childCount; i++)
+        {
+            Destroy(emailMessageList.transform.GetChild(i).gameObject);
+        }
+
+        for (int i = 0; i < messageList.Count; i++)
+        {
+            GameObject messageObj = Instantiate(message, emailMessageList.transform);
+            messageObj.GetComponent<RectTransform>().anchoredPosition = new Vector3(0, -47 - 20 * i, 0);
+            messageObj.transform.GetChild(0).GetComponent<TMP_Text>().SetText(messageList[i].senderOrRecipient);
+            messageObj.transform.GetChild(1).GetComponent<TMP_Text>().SetText(messageList[i].subject);
+            messageObj.transform.GetChild(2).GetComponent<TMP_Text>().SetText(messageList[i].time);
+
+            //Must make a copy of i, else the EmailItemEvent will break
+            int index = i;
+            messageObj.transform.GetChild(3).GetComponent<Button>().onClick.AddListener(() => EmailItemEvent(messageList, index));
+        }
+    }
+    async void InitializeMessages()
+    {
+        inboxData = new List<Message>();
+        sentData = new List<Message>();
+        spamData = new List<Message>();
+        trashData = new List<Message>();
+
+        XmlReaderSettings settings = new XmlReaderSettings();
+        settings.Async = true;
+
+        List<Message> currentData = inboxData;
+
+        //Forward only, that is, depth first
+        using (XmlReader reader = XmlReader.Create("Assets/Data/emailData.xml", settings))
+        {
+            while (await reader.ReadAsync())
+            {
+                if (reader.NodeType == XmlNodeType.Element)
+                {
+                    switch (reader.Name)
+                    {
+                        case "category":
+                            switch (reader.GetAttribute("type"))
+                            {
+                                case "inbox":
+                                    currentData = inboxData;
+                                    break;
+                                case "sent":
+                                    currentData = sentData;
+                                    break;
+                                case "spam":
+                                    currentData = spamData;
+                                    break;
+                                case "trash":
+                                    currentData = trashData;
+                                    break;
+                            }
+                            break;
+                        case "message":
+                            string senderOrRecipient = reader.GetAttribute("senderOrRecipient");
+                            string subject = reader.GetAttribute("subject");
+                            string time = reader.GetAttribute("time");
+                            string text = reader.GetAttribute("text");
+
+                            currentData.Add(new Message(senderOrRecipient, subject, time, text));
+                            break;
+                    }
+                }
+            }
+        }
+
+        //Default
+        InstantiateMessages(inboxData);
+    }
+    public void OnClickCompose()
+    {
+        emailComposeWindow.SetActive(true);
+    }
+
+    public void OnClickSend()
     {
         //Sequentially check if any conditions failed
         bool conditionsPassed = true;
