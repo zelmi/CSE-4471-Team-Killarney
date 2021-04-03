e0432d6a09f3eb326c9a67d07b41ce9ddc7075f2
Speno <gorgonfire@gmail.com>
2021-04-03 18:58:24 -0400

    Added Door animations

---
+++ Project/Assets/Scripts/Interactables/DoorInteractions.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorInteractions : MonoBehaviour, IInteractable
+{
+    [Header("Data Objects")]
+    [SerializeField] private string lockedHoverMessage;
+    [SerializeField] private string unlockedHoverMessage;
+    [SerializeField] private string scene;
+    private bool isInteractable;
+    private Animator _anim;
+    private bool isLocked;
+
+    public bool IsInteractable { get => isInteractable; }
+
+    public bool IsLocked { get => isLocked; set => isLocked = value; }
+
+    public string HoverMessage { 
+        get {
+            if(isLocked){
+                return lockedHoverMessage;
+            } else {
+                return unlockedHoverMessage;
+            }
+        }
+    }
+
+    public void onInteract()
+    {
+        if(isLocked){
+            PuzzleSceneManager.SwitchToPuzzle(scene);
+        } else {
+            _anim.SetBool("hasBeenOpened", true);
+            isInteractable = false;
+        }
+    }
+
+    void Start()
+    {
+        isInteractable = true;
+        _anim = this.transform.GetComponent<Animator>();
+        isLocked = false;
+    }
+}
